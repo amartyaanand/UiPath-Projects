67272acd1be0aea534bbdd5d1c9bcb9a819e57cc
Amartya Anand <amartyaanand21@live.com>
2020-11-27 22:36:21 +0530

    [Amartya] Add : Project based on UiPath.

---
+++ AddressBookSystem/AddressBookQuery.sql
@@ -0,0 +1,16 @@
+Create database AddressBookSystem;
+use AddressBookSystem;
+
+create table AddressBook (
+firstName varchar(30),
+lastName varchar(30),
+fatherName varchar(30),
+motherName varchar(30),
+bloodGroup varchar(30),
+address varchar(30),
+city varchar(30),
+state varchar(30),
+zip varchar(30),
+phoneNumber varchar(30),
+email varchar(30));
+
